af260fb20fec4cc09fe738c035ad0d90f0408f3e
san <skysan@gmail.com>
2016-12-18 20:04:21 +0530

    added water level sensor on Analog pin A2 and renamed SOLENOID_STATE to power_to_solenoid and renamed dalas sensors variable to tempSensor

--- util.h
+++ util.h
@@ -1,7 +1,52 @@
 #include <dht.h>
+#include "Wire.h"
 //#include <cstring.h>
 namespace util
 {
+
+void printProgStr(PGM_P str)
+{
+  char c;
+  if (!str) return;
+  while ((c = pgm_read_byte(str++)))
+    //    Serial.print(c, BYTE);
+    Serial.write(c);
+}
+
+// Convert normal decimal numbers to binary coded decimal
+byte decToBcd(byte val)
+{
+  return ( (val / 10 * 16) + (val % 10) );
+}
+// Convert binary coded decimal to normal decimal numbers
+byte bcdToDec(byte val)
+{
+  return ( (val / 16 * 10) + (val % 16) );
+}
+
+
+
+void readDS3231time(byte *second,
+                    byte *minute,
+                    byte *hour,
+                    byte *dayOfWeek,
+                    byte *dayOfMonth,
+                    byte *month,
+                    byte *year)
+{
+  Wire.beginTransmission(DS3231_I2C_ADDRESS);
+  Wire.write(0); // set DS3231 register pointer to 00h
+  Wire.endTransmission();
+  Wire.requestFrom(DS3231_I2C_ADDRESS, 7);
+  // request seven bytes of data from DS3231 starting from register 00h
+  *second = bcdToDec(Wire.read() & 0x7f);
+  *minute = bcdToDec(Wire.read());
+  *hour = bcdToDec(Wire.read() & 0x3f);
+  *dayOfWeek = bcdToDec(Wire.read());
+  *dayOfMonth = bcdToDec(Wire.read());
+  *month = bcdToDec(Wire.read());
+  *year = bcdToDec(Wire.read());
+}
 
 double* getTempHumdata(const int sensorPin)
 {
--- webServer_Displaying_everything_v01.ino
+++ webServer_Displaying_everything_v01.ino
@@ -1,59 +1,65 @@
 
 #include <SPI.h>
+#include "Wire.h"
+#define DS3231_I2C_ADDRESS 0x68
+
 #include <Ethernet.h>
+#include <elapsedMillis.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include "Dewpnt_heatIndx.h"
 #include "util.h"
-#include<dht.h>
+#include <dht.h>
 dht DHT;
 
+const char htmlStyleMultiline[] PROGMEM = "<style>"
+    ".tooltip {"
+    "    position: relative;"
+    "    display: inline-block;"
+    "   border-bottom: 1px dotted black;"
+    "}"
 
-const char htmlStyleMultiline[] = "<style>"
-                                  ".tooltip {"
-                                  "    position: relative;"
-                                  "    display: inline-block;"
-                                  "   border-bottom: 1px dotted black;"
-                                  "}"
+    ".tooltip .tooltiptext {"
+    "   visibility: hidden;"
+    "   width: 120px;"
+    "   background-color: black;"
+    "   color: #fff;"
+    "   text-align: center;"
+    "    border-radius: 6px;"
+    "    padding: 5px 0;"
 
-                                  ".tooltip .tooltiptext {"
-                                  "   visibility: hidden;"
-                                  "   width: 120px;"
-                                  "   background-color: black;"
-                                  "   color: #fff;"
-                                  "   text-align: center;"
-                                  "    border-radius: 6px;"
-                                  "    padding: 5px 0;"
+    "    /* Position the tooltip */"
+    "    position: absolute;"
+    "    z-index: 1;"
+    "}"
 
-                                  "    /* Position the tooltip */"
-                                  "    position: absolute;"
-                                  "    z-index: 1;"
-                                  "}"
-
-                                  ".tooltip:hover .tooltiptext {"
-                                  "    visibility: visible;"
-                                  "}"
-                                  "</style>";
+    ".tooltip:hover .tooltiptext {"
+    "    visibility: visible;"
+    "}"
+    "</style>";
 
 
-void printProgStr(PGM_P const str[])
+
+int getHour()
 {
-  char c;
-  if (!str) return;
-  while ((c = pgm_read_byte(str++)))
-    //    Serial.print(c, BYTE);
-    Serial.write(c);
+  byte second, minute, hour, dayOfWeek, dayOfMonth, month, year;
+  // retrieve data from DS3231
+  util::readDS3231time(&second, &minute, &hour, &dayOfWeek, &dayOfMonth, &month,
+                       &year);
+  return hour;
 }
 
 
-bool SOLENOID_STATE = false;
 
-#define ONE_WIRE_BUS 42 /*-(Connect to Pin 2 )- brpwn wire out on potentiometer*/
+
+bool power_to_solenoid = false;
+
+#define ONE_WIRE_BUS 42 /*-(Connect to Pin 42 )- brown wire out on potentiometer*/
 /* Set up a oneWire instance to communicate with any OneWire device*/
 OneWire ourWire(ONE_WIRE_BUS);
 
 /* Tell Dallas Temperature Library to use oneWire Library */
-DallasTemperature sensors(&ourWire);
+DallasTemperature tempSensor(&ourWire);
 
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
@@ -75,22 +81,13 @@ const int8_t DRY_SOIL_DEFAULT = 25;
 // ///////////// PIN Setup /////////////////
 const int solenoidPin = 6;    // D6 : This is the output pin on the Arduino we are using
 #define DHT11_PIN 8 // D8 to 2nd pin on DHT, leave 3rd pin unconnected 4th is gnd
-
 const int8_t rainsense = 7; // analog sensor input pin A7
 const int buzzerout = 2; // digital output pin D2 - buzzer output
-//int countval = 0; // counter value starting from 0 and goes up by 1 every second
-
-
-// int GLED = 5; // Wet Indicator at Digital PIN D5
-// int RLED = 9; // Dry Indicator at Digital PIN D9
+const int8_t pwatLvlSense = A2; // Pot Water Level Sensor A2 connected with blue wire
 int SENSE = 1; // Soil Sensor input at Analog PIN A1 Green wire
 int val = 0;
 
-// int greenLedPin = 2; // LED on pin 2
-// int yellowLedPin = 3;
-// int redLedPin = 4;
-
-int lightSensorPin = A8;
+int lightSensorPin = A3; // earlier it was A8
 int analogValue = 0;
 
 double tempInC;
@@ -104,11 +101,20 @@ double hIinCel;
 
 String soilMsg;
 
+elapsedMillis timer0;
+
+// the interval in mS
+#define interval 45000
+
 void setup() {
+  Wire.begin();
+
+  power_to_solenoid = false;
+  timer0 = 0; // clear the timer at the end of startup
   pinMode(solenoidPin, OUTPUT); // Sets the pin as an output
   pinMode(buzzerout, OUTPUT);
   pinMode(rainsense, INPUT);
-  sensors.begin();
+  tempSensor.begin();
   // start the Ethernet connection and the server:
   Ethernet.begin(mac, ip);
   server.begin();
@@ -123,7 +129,7 @@ void setup() {
 }
 
 void loop() {
-
+  int watrLvlSnsr = analogRead(pwatLvlSense);
   val = analogRead(SENSE);
   val = val / 10;
 
@@ -132,9 +138,9 @@ void loop() {
   //  tempInC = util::getTempHumdata(DHT11_PIN)[0];
   //  humidity = util::getTempHumdata(DHT11_PIN)[1];
   int chk = DHT.read11(DHT11_PIN);
-  sensors.requestTemperatures(); // Send the command to get temperatures
-  tempInC = sensors.getTempCByIndex(0);
-  tF = sensors.getTempFByIndex(0);
+  tempSensor.requestTemperatures(); // Send the command to get temperatures
+  tempInC = tempSensor.getTempCByIndex(0);
+  tF = tempSensor.getTempFByIndex(0);
   humidity = DHT.humidity;
   dP = (Dewpnt_heatIndx::dewPointFast(tempInC, humidity));
   dPF = ((dP * 9) / 5) + 32;
@@ -149,35 +155,52 @@ void loop() {
 
   if (rainSenseReading < 500)  {
     rainMsg = F("It is raining heavily !");
+    power_to_solenoid = false;
   }
   if (rainSenseReading < 300) {
     rainMsg = F("Moderate rain.");
+    power_to_solenoid = false;
   }
   if (rainSenseReading < 200) {
     rainMsg = F("Light Rain Showers !");
+    power_to_solenoid = false;
   }
   if (rainSenseReading > 500) {
     rainMsg = F("Not Raining.");
-    SOLENOID_STATE = true;
+    power_to_solenoid = true;
   }
-  else SOLENOID_STATE == false;
+  else power_to_solenoid = false;
 
+  if ((getHour() >= 19  && getHour() <= 23) || (getHour() >= 0 && getHour() <= 7)) {
+    power_to_solenoid = false;
+  }
+
+  if (watrLvlSnsr >=400){
+    power_to_solenoid = false;
+  }
 
   // Turn on Solenoid Valve if soil moisture value less than 25
-  if (val < DRY_SOIL_DEFAULT && SOLENOID_STATE == true) {
-    // TODO : if soil is dry turn on solenoid for 45 seconds (45000 miliseconds)
+  if (val < DRY_SOIL_DEFAULT && power_to_solenoid == true) {
     soilMsg = F("Soil is dry. ");
     // do not water pot if its raining or if it is night.
-    if (rainMsg != "Not Raining" || analogValue > 300) {
+    if (analogValue > 300) {
       soilMsg = soilMsg +  F("Watering the plant.");
+      // turn solenoid off after 45 sec
       digitalWrite(solenoidPin, HIGH);
       digitalWrite(buzzerout, HIGH);
+      if ((!power_to_solenoid) && (timer0 > interval)) {
+        power_to_solenoid = false; // don't execute this again
+        digitalWrite(solenoidPin, LOW);
+        digitalWrite(buzzerout, LOW);
+
+      }
+
     }
   }
   else {
     digitalWrite(solenoidPin, LOW);
     soilMsg = F("Soil is damp.");
-    SOLENOID_STATE = false;
+    power_to_solenoid = false;
     digitalWrite(buzzerout, LOW);
   }
 
@@ -209,21 +232,18 @@ void loop() {
           client.println(F("<!DOCTYPE HTML>"));
           client.println(F("<html><head><title>"));
           client.println(F("Welcome to Arduino WebServer</title>"));
-//                    printProgStr( htmlStyleMultiline );
+          //          util::printProgStr( htmlStyleMultiline );
           client.println(F("</head><body style='background-color:grey'>"));
           client.println(c);
           client.println(F("<p style='color:red';style='font-family: Arial'>LIVE</p>"));
 
           if (rainSenseReading < 500) {
-            //            client.print(F(
             client.print(rainMsg);
             client.print(F(" Rain Sensor reads: "));
             client.println(rainSenseReading);
           }
           else client.println();
-
           client.print(F("<br>Temperature: <u>+</u>"));
-
           client.print(tempInC, 1);
           client.print(F("&#8451; / "));
           client.print(tF);
@@ -249,21 +269,22 @@ void loop() {
             client.println(F(" &#8457; <br>"));
           }
 
-          client.print(F("<br>Light Sensor reads "));
-          client.print(analogValue);
           if (analogValue < 10) {
-            client.println(F(", it is  <font style='color:red';>dark</font>."));
+            client.println(F("<div class='tooltip'>It is  <font style='color:red';>dark</font>."));
           }
           else if (analogValue > 10 && analogValue < 50) {
-            client.println(F(", Fair amount of<font style='color:yellow';><b>light</b></font>, but not very bright"));
+            client.println(F("<div class='tooltip'>Fair amount of<font style='color:yellow';><b>light</b></font>, but not very bright"));
           }
           else if (analogValue >= 50 && analogValue <= 100) {
-            client.println(F(", Fair amount of <font style='color:yellow';><b>light</b></font>, but not bright enough."));
+            client.println(F("<div class='tooltip'>Fair amount of <font style='color:yellow';><b>light</b></font>, but not bright enough."));
           }
           else {
-            client.println(F(", Full Bright Day<font style='color:green';><b> :) </b></font>."));
+            client.println(F("<div class='tooltip'>Full Bright Day<font style='color:green';><b> :) </b></font>."));
           }
 
+          client.print(F("<span class='tooltiptext'> LDR Value Reads: "));
+          client.print(analogValue);
+          client.println(F("</span>"));
           client.print(F("<br>Pot Soil Moisture: "));
           client.print(val);
           client.print(F("&nbsp;"));
